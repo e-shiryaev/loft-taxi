f3ba2c4e58566ded8657e81740a028054ed678f2
siryaev <shiryaev@proitr.ru>
2022-03-24 14:06:57 +0500

    Доработал тест на миддл вар

--- src/__test__/authMiddleware.spec.js
+++ src/__test__/authMiddleware.spec.js
@@ -2,15 +2,13 @@ import {authMiddleware} from "../milldewares/authMiddleware";
 import {auth, LOG_IN} from "../actions";
 import {serverLogin} from "../loftTaxiServer";
 
-jest.mock('../loftTaxiServer', () => ({serverLogin: jest.fn(() => {
-  return {
-    success: true,
-    token: 'sdf'
-  }})}));
+jest.mock('../loftTaxiServer', () => ({serverLogin: jest.fn()}));
 
 describe('autMiddleware', () => {
   describe('#auth', () => {
     it ('auth through loftTaxiServer', async () => {
+      serverLogin.mockImplementation(() => ({ success: true, token: 'abc' }))
+
       const dispatch = jest.fn();
       const login = 'test@test.com';
       const password = '123123';
@@ -19,7 +17,7 @@ describe('autMiddleware', () => {
         auth(login, password)
       );
       expect(serverLogin).toBeCalledWith(login, password);
-      expect(dispatch).toBeCalledWith({type: LOG_IN});
+      expect(dispatch).toBeCalledWith({payload: {token: 'abc'}, type: LOG_IN});
     });
   })
 })
